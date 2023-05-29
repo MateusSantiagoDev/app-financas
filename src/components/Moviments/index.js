19dc18ec9bad6bea5872c5b6f07f4a04a065ffd8
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-05-29 13:11:29 -0300

    feat: add moti in moviments component

--- src/components/Moviments/index.js
+++ src/components/Moviments/index.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { MotiView, MotiText, AnimatePresence } from "moti";
 
 export function Moviments({ data }) {
   const [showValue, setShowValue] = useState(false);
@@ -14,11 +15,32 @@ export function Moviments({ data }) {
         <Text style={styles.label}>{data.label}</Text>
 
         {showValue ? (
-          <Text style={data.type === 1 ? styles.value : styles.expenses}>
-            {data.type === 1 ? `R$ ${data.value}` : `R$ -${data.value}`}
-          </Text>
+          <AnimatePresence exitBeforeEnter>
+            <MotiText
+              style={data.type === 1 ? styles.value : styles.expenses}
+              from={{
+                translateX: 100,
+              }}
+              animate={{
+                translateX: 0,
+              }}
+              transition={{
+                type: "timing",
+                direction: 500,
+              }}
+            >
+              {data.type === 1 ? `R$ ${data.value}` : `R$ -${data.value}`}
+            </MotiText>
+          </AnimatePresence>
         ) : (
-          <View style={styles.skeleton}></View>
+          <AnimatePresence exitBeforeEnter>
+            <MotiView
+            style={styles.skeleton}
+            from={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ type: "timing" }}
+            ></MotiView>
+          </AnimatePresence>
         )}
       </View>
     </TouchableOpacity>
