bc674a2822546add757963b43dab3eb2d90bfa84
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-05-29 12:15:06 -0300

    feat: create moviments component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { Header } from '../../components/Header';
-import { Balance } from '../../components/balance'; 
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import { Header } from "../../components/Header";
+import { Balance } from "../../components/balance";
+import { Moviments } from "../../components/Moviments";
+
+const list = [
+  {
+    id: 1,
+    label: "Boleto conta de luz",
+    value: "300,90",
+    date: "17/09/2022",
+    type: 0, // despesas
+  },
+  {
+    id: 2,
+    label: "PIX cliente X",
+    value: "2.500,00",
+    date: "22/09/2022",
+    type: 1, // receita / entradas
+  },
+  {
+    id: 3,
+    label: "Salario",
+    value: "7.200,00",
+    date: "25/09/2022",
+    type: 1, // receita / entradas
+  },
+];
 
 export function Home() {
   return (
     <View style={styles.container}>
-      <Header name="jose rafael"/>
-      <Balance saldo="9.250,90" gastos="-527,00"/>
+      <Header name="jose rafael" />
+      <Balance saldo="9.250,90" gastos="-527,00" />
 
+      <Text style={styles.title}>Últimas movimentações</Text>
+
+      <FlatList
+      style={styles.list}
+      data={list}
+      keyExtractor={ (item) => String(item.id) }
+      showsVerticalScrollIndicator={false}
+      renderItem={({ item }) => <Moviments data={item}/>}
+      />
     </View>
   );
 }
@@ -15,6 +49,17 @@ export function Home() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fafafa',
+    backgroundColor: "#fafafa",
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginLeft: 14,
+    marginRight: 14,
+    marginTop: 14,
+  },
+  list: {
+    marginStart: 14,
+    marginEnd: 14,
   },
 });
